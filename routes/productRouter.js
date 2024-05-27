a9067bf304dfa5d94d4c30a0a93a319cc0630959
luischehin <luischehinok@gmail.com>
2024-05-27 13:16:14 -0300

    productos con los metodos get put post delete creado

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -36,6 +36,74 @@ router.get('/:pid', (req, res) => {
     if (product) {
         res.json(product);
     } else {
-        res.status(404).json({ message: 'Product not found' });
+        res.status(404).json({ message: 'Producto no encontrado' });
     }
 });
+
+// creo un nuevo producto 
+router.post('/', (req, res) => {
+    const { titulo, descripcion, codigo, precio, estado = true, stock, categoria, fotos = [] } = req.body;
+    if (!title || !descripcion || !codigo || !precio || !stock || !categoria) {
+        return res.status(400).json({ message: 'Todos los campos a excepcion de fotos, es obligatorio' });
+    }
+    const products = readProducts();
+    const newProduct = {
+        id: (products.length + 1).toString(),
+        titulo,
+        descripcion,
+        codigo,
+        precio,
+        estado,
+        stock,
+        categoria,
+        foto
+    };
+    products.push(newProduct);
+    writeProducts(products);
+    res.status(201).json(newProduct);
+
+
+});
+
+// actualizar un producto 
+
+router.put('/:pid', (req, res) => {
+    const { pid } = req.params;
+    const { titulo, descripcion, codigo, precio, estado, stock, categoria, foto } = req.body;
+    const products = readProducts();
+    const productIndex = products.findIndex(p => p.id === pid);
+    if (productIndex !== -1) {
+        const updatedProduct = {
+            ...products[productIndex],
+            titulo,
+            descripcion,
+            codigo,
+            precio,
+            estado,
+            stock,
+            categoria,
+            foto
+        };
+        products[productIndex] = updatedProduct;
+        writeProducts(products);
+        res.json(updatedProduct);
+    } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+    }
+});
+
+// Borrar un producto
+
+router.delete('/:pid', (req, res) => {
+    const { pid } = req.params;
+    const products = readProducts();
+    const newProducts = products.filter(p => p.id !== pid);
+    if (products.length !== newProducts.length) {
+        writeProducts(newProducts);
+        res.json({ message: 'producto borrado' });
+    } else {
+        res.status(404).json({ message: 'producto no encontrado' });
+    }
+});
+
+module.exports = router;
