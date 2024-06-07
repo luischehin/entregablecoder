5d3eb0d2f2b13fee9044ff6c24804d2a50704f5c
luischehin <luischehinok@gmail.com>
2024-06-07 15:14:09 -0300

    corregi algunos errores respectos a la entrega anterior

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const bodyParser = requiere('body-parser');
+import Express from "express"
+import productsRoutes from "./routes/products.routes.js"
+import cartRoutes from "./routes/carts.routes.js"
 
-const app = express();
+const app = Express();
+
 const PORT = 8080;
 
-app.use = (bodyParser.json());
-
-// rutas
-const productoRouter = requiere('./routes/productRouter');
-const cartRouter = require('./routes/cartRouter');
-
-app.use('/api/products', productoRouter);
-app.use('/api/carts', cartRouter);
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`serivdr andando puerto ${PORT}`)
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
 })
+
+app.use('/api/products', productsRoutes);
+app.use('/api/carts', cartRoutes);
